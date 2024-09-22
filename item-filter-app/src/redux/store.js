ecda381efb97e9f2629030fd383a935c53be7595
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-09-22 03:41:55 +0000

    comments added

--- item-filter-app/src/redux/store.js
+++ item-filter-app/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemsReducer from './itemsSlice';
 
+
+//redux store
 const store = configureStore({
     reducer:{
-        items: itemsReducer
+        items: itemsReducer //item state manage here
     }
 });
 
